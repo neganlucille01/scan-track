dbe481242c5d51a0a04b39535394396840bc3727
Apurba Koley <apurba@Apurbas-MacBook-Pro.local>
2024-08-11 16:01:20 +0530

    Next navigation used and component structure created

--- src/app/page.js
+++
@@ -1,8 +0,0 @@
-import Image from "next/image";
-import styles from "./page.module.scss";
-import { quote } from "../../Styles/Global";
-export default function Home() {
-  return <>
-  {quote}
-  </>
-}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,14 @@
 
 "use client";
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './Login';
-import Dashboard from './Dashboard';
+import Login from './(pages)/login/page';
+
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/Dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
+   <>
+   <Login/>
+   </>
   );
 };
 
